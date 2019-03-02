92854f7f7260d64bb29722806155f78eb8303979
Yudel Martinez <yam3nv@virginia.edu>
2019-03-02 16:45:25 -0500

    Cleanup

--- app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/MainActivity.java
+++ app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/MainActivity.java
@@ -2,18 +2,20 @@ package com.linklab.emmanuelogunjirin.besi_c;
 
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private Button EMA;
+    private Button Sleep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        EMA = (Button) findViewById(R.id.EMA);
+        Sleep = (Button) findViewById(R.id.Sleep);
 
         // Enables Always-on
         setAmbientEnabled();
