5f025198a30d57d328f2f0e69368154b97a492d8
Yudel Martinez <yam3nv@virginia.edu>
2019-03-05 02:19:18 -0500

    Revert "Can Successfully start HR Service but Cannot End"
    This reverts commit 527a5d414d29aca698171b5163e3ebe380f4ce1b.

--- app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/SensorData.java
+++ app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/SensorData.java
@@ -1,11 +1,9 @@
 package com.linklab.emmanuelogunjirin.besi_c;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -15,8 +13,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.wearable.activity.WearableActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class SensorData extends WearableActivity implements SensorEventListener {
@@ -25,40 +21,14 @@ public class SensorData extends WearableActivity implements SensorEventListener 
     private SensorManager mSensorManager;
     private Sensor mHeartRate;
 
-    private Button button;
-    private int counter = 0;
+    public HeartRateMonitor hr = new HeartRateMonitor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor_data);
 
-        button = findViewById(R.id.button);
-
-        final Intent HRService = new Intent(this, HeartRateMonitor.class);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (counter%2==0)
-                {
-                    button.setText("Stop");
-                    startService(HRService);
-                    button.setBackgroundColor(0xFF252);
-                }
-                else
-                {
-                    button.setText("Start");
-                    stopService(HRService);
-                    button.setBackgroundColor(0x00C853);
-                }
-                counter ++;
-
-            }
-        });
-
-
+        startService(new Intent(this, HeartRateMonitor.class));
 
         // Enables Always-on
         setAmbientEnabled();
