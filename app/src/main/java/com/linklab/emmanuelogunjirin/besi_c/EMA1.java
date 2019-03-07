435f823ff8022bf37c67e2271ae5afb5a539a0ce
Yudel Martinez <yam3nv@virginia.edu>
2019-03-07 01:17:08 -0500

    Improved System of EMA questions and data persistance

--- app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/EMA1.java
+++ app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/EMA1.java
@@ -15,17 +15,40 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class EMA1 extends WearableActivity       // This is the main activity for the questions
 {
-    private int qcount = 5;     // This is the amount of questions to be shown.
     private Button res, back, next;     // These are the buttons shown on the screen to navigate the watch
     private TextView req;   // This is a text view for the question
     private int resTaps = 0;
     private ArrayList<String> responses = new ArrayList<>();    // This is a string that is appended to.
-    private String[] UserResponses = new String[qcount];
+    private String[] UserResponses;
+    private int[] UserResponseIndex;
     public Vibrator v;      // The vibrator that provides haptic feedback.
+
+    private int FollowUpEMADelay = 5*60*1000; //Time before followup EMA / EMA2 following submission
+
+
+    private int CurrentQuestion = 0;
+    private Map<String,String[]> EMAPromts = new HashMap<>();
+    private String[] Questions =
+            {"Is patient having pain now?",
+            "What is patient's pain level?",
+            "How distressed are you?",
+            "How distressed is the patient?",
+            "Did patient take an opioid for the pain?"};
+    private String[][] Answers = {
+            {"Yes", "No"},
+            {"1","2","3","4","5","6","7","8","9","10"},
+            {"Not at all", "A little", "Moderately", "Very"},
+            {"Not at all", "A little", "Moderately", "Very"},
+            {"Yes", "No"}};
+    private int tapCounter = 0;
 
 
     @Override
@@ -56,196 +79,98 @@ public class EMA1 extends WearableActivity       // This is the main activity fo
             }
         });
 
-        q1();       // Moves on to question 1
+        UserResponses = new String[Questions.length];
+        UserResponseIndex = new int[UserResponses.length];
+        //q1();       // Moves on to question 1
+
+        QuestionSystem();
 
         // Enables Always-on
         setAmbientEnabled();
     }
 
 
-    private void Cycle_Responses()
+    private int Cycle_Responses()
     {
-        res.setText(responses.get(resTaps%responses.size()));
+        int index = resTaps%responses.size();
+        res.setText(responses.get(index));
+        return index;
     }
 
-    // This is Question 1
-    @SuppressLint("SetTextI18n")
-    /* The survey begins with Question 1 here */
-    private void q1()
+    private void LogActivity()
     {
-        back.setText("Cancel");
-        resTaps = 0;
-        String question = "Is patient having pain now?";
-        responses.clear();
-        responses.add("Yes");
-        responses.add("No");
-
-        req.setText(question);
-        Cycle_Responses();
-        // If the next button is clicked
-        next.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)      // Haptic Feedback
-            {
-                v.vibrate(20);
-                UserResponses[0] = res.getText().toString();
-                if (UserResponses[0].equals("Yes"))     // If the answer to is "yes", moves on to question 2
-                {
-                    q2();
-                }
-                else
-                {
-                    Cancel();    // Else, it closes the question screen.
-                }
-            }
-        });
-
-        // If the back button is clicked
-        back.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);      // Haptic Feedback
-                Cancel();
-            }
-        });
+        String data =  (new Utils().getTime()) + ",EMA_Pain," + String.valueOf(CurrentQuestion) + "," + UserResponses[CurrentQuestion];
+        DataLogger datalog = new DataLogger("EMA_Activity.csv",data);
+        datalog.LogData();
     }
 
-    // This is Question 2 (See question one for other comments, they follow similar structure)
-    @SuppressLint("SetTextI18n")
-    private void q2()
+
+    private void QuestionSystem()
     {
-        back.setText("Back");
-        resTaps = 0;
-        String question = "What is patient's pain level?";
-        responses.clear();
-        for (int i=1; i<=10; i++)
+        if (CurrentQuestion < Questions.length)
         {
-            responses.add(""+i);
+            resTaps = UserResponseIndex[CurrentQuestion];
+            req.setText(Questions[CurrentQuestion]);
+            responses.clear();
+            for(int i=0; i < Answers[CurrentQuestion].length; i++)
+            {
+                responses.add(Answers[CurrentQuestion][i]);
+            }
+            Cycle_Responses();
+
+            next.setOnClickListener( new View.OnClickListener()
+            {
+                public void onClick(View view)      // Haptic Feedback
+                {
+                    v.vibrate(20);
+                    UserResponses[CurrentQuestion] = res.getText().toString();
+                    UserResponseIndex[CurrentQuestion] = Cycle_Responses();
+                    LogActivity();
+                    if (UserResponses[0].equals("Yes"))     // If the answer to is "yes", moves on to question 2
+                    {
+                        CurrentQuestion++;
+                        QuestionSystem();
+                    }
+                    else
+                    {
+                        Cancel();    // Else, it closes the question screen.
+                    }
+                }
+            });
+
+            // If the back button is clicked
+            back.setOnClickListener( new View.OnClickListener()
+            {
+                public void onClick(View view)
+                {
+                    v.vibrate(20); // Haptic Feedback
+                    UserResponses[CurrentQuestion] = res.getText().toString();
+                    UserResponseIndex[CurrentQuestion] = Cycle_Responses();
+                    LogActivity();
+                    if (CurrentQuestion == 0)
+                    {Cancel();}
+                    else
+                    {
+                        CurrentQuestion --;
+                        QuestionSystem();
+                    }
+                }
+            });
         }
-        req.setText(question);
-        Cycle_Responses();
-        back.setOnClickListener( new View.OnClickListener()
+        else
         {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[1] = res.getText().toString();
-                q1();   // Goes back to question 1
-            }
-        });
-        next.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[1] = res.getText().toString();
-                q3();   // Goes back to question 3
-            }
-        });
+            Submit();
+        }
     }
 
-    // This is Question 3 (See question one for other comments, they follow similar structure)
-    private void q3()
+    private void ThankYou()
     {
-        resTaps = 0;
-        String question = "How distressed are you?";
-        responses.clear();
-
-        // Response Options
-        responses.add("Not at all");
-        responses.add("A little");
-        responses.add("Moderately");
-        responses.add("Very");
-
-        req.setText(question);
-        Cycle_Responses();
-        back.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[2] = res.getText().toString();
-                q2();   // Goes back to question 2
-            }
-        });
-        next.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[2] = res.getText().toString();
-                q4();   // Goes on to question 4
-            }
-        });
-    }
-
-    // This is Question 4 (See question one for other comments, they follow similar structure)
-    private void q4()
-    {
-        resTaps = 0;
-        String question = "How distressed is the patient?";
-        responses.clear();
-
-        //Response Options
-        responses.add("Not at all");
-        responses.add("A little");
-        responses.add("Moderately");
-        responses.add("Very");
-
-        req.setText(question);
-        Cycle_Responses();
-        back.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[3] = res.getText().toString();
-                q3();   // Goes back to question 3
-            }
-        });
-        next.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[3] = res.getText().toString();
-                q5();   // Goes on to question 4
-            }
-        });
-    }
-
-    // This is Question 5 (See question one for other comments, they follow similar structure)
-    private void q5()
-    {
-        resTaps = 0;
-        String question = "Did patient take an opioid for the pain?";
-        responses.clear();
-
-        responses.add("Yes");
-        responses.add("No");
-        responses.add("Unsure");
-
-        req.setText(question);
-        Cycle_Responses();
-        back.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[4] = res.getText().toString();
-                q4();   // Goes back to question 4
-            }
-        });
-        next.setOnClickListener( new View.OnClickListener()
-        {
-            public void onClick(View view)
-            {
-                v.vibrate(20);
-                UserResponses[4] = res.getText().toString();
-                Submit();   // Submits the responses.
-            }
-        });
+        Context context = getApplicationContext();
+        CharSequence text = "Thank You!";       // Pop up information to the person
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);          // A short message at the end to say thank you.
+        toast.show();
+        finish();
     }
 
     /* This is the end of survey part. It submits the data. */
@@ -263,17 +188,13 @@ public class EMA1 extends WearableActivity       // This is the main activity fo
         /* Logs the data in a csv format */
         DataLogger dataLogger = new DataLogger("EMA1_Results.csv", log.toString());
         dataLogger.LogData();
-        Context context = getApplicationContext();
-        CharSequence text = "Thank You!";       // Pop up information to the person
+        ThankYou();
 
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);          // A short message at the end to say thank you.
-        toast.show();
-        finish();
     }
 
     private void Cancel()
     {
+        ThankYou();
         finish();   // Closes the entire survey
     }
 }
