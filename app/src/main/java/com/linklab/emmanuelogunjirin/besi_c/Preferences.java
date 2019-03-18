e700feaff9b1f5edb4d656a592574c1e09a8ec0a
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-03-18 13:36:44 -0400

    Removed typo error.

--- app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/Preferences.java
+++ app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/Preferences.java
@@ -40,4 +40,3 @@ public class Preferences
 //    public String Pedometer_Data_Headers = ""; // Column Headers for Pedometer_Data
 //    public String Estimote_Data_Headers = ""; // Column Headers for Estimote_Data
 }
-P
