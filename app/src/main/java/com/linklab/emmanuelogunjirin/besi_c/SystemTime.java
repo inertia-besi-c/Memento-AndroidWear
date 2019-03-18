d64ad8610414284e6b15756c9669731e0a15906f
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-03-18 13:28:56 -0400

    Fully Commented SystemTime Service.

--- app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/SystemTime.java
+++ app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/SystemTime.java
@@ -1,16 +1,17 @@
 package com.linklab.emmanuelogunjirin.besi_c;
 
+// Imports
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-class SystemTime     // Things that we need in the future and do not want to lose.
+class SystemTime     // Class that acquires the current time from the system and saves it.
 {
-    String getTime()
+    String getTime()        // Puts the system time acquired into the desired format wanted.
     {
-        DateFormat datetimeFormat = new SimpleDateFormat("MM/dd/yyy HH:mm:ss.SSS", Locale.US);
-        Date current = new Date();
-        return datetimeFormat.format(current);
+        DateFormat datetimeFormat = new SimpleDateFormat("MM/dd/yyy HH:mm:ss.SSS", Locale.US);      // Specified format of the time, in US style.
+        Date current = new Date();      // Calls the current date from the system.
+        return datetimeFormat.format(current);  // Returns the date and time the system is in.
     }
 }
