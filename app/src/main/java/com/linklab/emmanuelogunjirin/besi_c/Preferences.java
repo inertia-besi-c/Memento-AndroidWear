d3a581a3d1ae79cffefc6ce4fcdede8226f2af8b
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-03-18 13:35:22 -0400

    Fully Commented Preferences Service
    Commented out the unused log lines....

--- app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/Preferences.java
+++ app/src/main/java/com/linklab/emmanuelogunjirin/besi_c/Preferences.java
@@ -1,11 +1,11 @@
 package com.linklab.emmanuelogunjirin.besi_c;
 
+// Imports
+//import android.util.Log;
+
 public class Preferences
 {
-    // Role of user wearing the watch
-    String Role = "CG"; // CG for Caregiver or PT for Patient
-
-    // For testing, we used one band holder as the patient, and two band holder as the caregiver
+    String Role = "CG";  // Role of user wearing the watch; CG for Caregiver or PT for Patient
 
     // Settings for Pain EMA | Time is in ms |
     public int PainEMAReminderDelay = 0; // How long to give the user before starting the timer to remind user to complete the ema
@@ -21,14 +21,14 @@ public class Preferences
     public int EoDEMA_Time_Hour = 21; // Hour at which the daily ema should go off
     public int EoDEMA_Time_Minute = 0; // Minute of hour at which daily ema should go off
     public int EoDEMA_Time_Second = 0; // Second of minute at which daily ema should go off
-    public int EoDEMA_Timer_Delay = 10 * 60 * 1000; //
-    public int EoDEMA_Period = 24*60*60*1000; //
+    public int EoDEMA_Timer_Delay = 10 * 60 * 1000; // This is how often the timer waits before firing again
+    public int EoDEMA_Period = 24*60*60*1000; // This is how often the timer waits before firing again
 
     // Settings for Heart Rate Monitoring | Time is in ms |
     long HRSampleDuration = 30000;   // How long should heart rate be measured each time?
     long HRMeasurementInterval = 5 * 60 * 1000;  // Every how often should a measurement be taken?
 
-//    // Settings for Log Files -- NOT YET IMPLEMENTED
+/* Settings for Log Files -- NOT YET IMPLEMENTED */
 //    public String EndOfDay_EMA_Activity_Headers = ""; // Column Headers for EndOfDay_EMA_Activity
 //    public String EndOfDay_EMA_Results_Headers = ""; // Column Headers for EndOfDay_EMA_Results
 //    public String Pain_EMA_Activity_Headers = ""; // Column Headers for Pain_EMA_Activity
@@ -39,6 +39,4 @@ public class Preferences
 //    public String Accelerometer_Data_Headers = ""; // Column Headers for Accelerometer_Data
 //    public String Pedometer_Data_Headers = ""; // Column Headers for Pedometer_Data
 //    public String Estimote_Data_Headers = ""; // Column Headers for Estimote_Data
-
-
 }
