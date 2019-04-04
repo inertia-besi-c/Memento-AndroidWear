caac498556b384b69acc2eb061e7393e1d130c72
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-04-04 14:05:47 -0400

    Fixed End of Day EMA Timer Service.

--- app/src/main/java/com/linklab/INERTIA/besi_c/EODTimerService.java
+++ app/src/main/java/com/linklab/INERTIA/besi_c/EODTimerService.java
@@ -4,7 +4,7 @@ package com.linklab.INERTIA.besi_c;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
-
+import android.util.Log;
 import java.util.Calendar;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,6 +14,8 @@ public class EODTimerService extends Application        // Starts the EOD EMA Ti
     @Override
     public void onCreate()      // Creates the instance when it is started.
     {
+        Log.i("End of Day EMA", "End of Day EMA Timer is starting");     // Logs on Console.
+
         super.onCreate();       // Starts the creation.
         ScheduleEndOfDayEMA(this);      // Links the schedule EOD EMA to this.
     }
@@ -27,7 +29,7 @@ public class EODTimerService extends Application        // Starts the EOD EMA Ti
         calendar.set(Calendar.MINUTE, new Preferences().EoDEMA_Time_Minute);     // Gets the minute of the day from the preference.
         calendar.set(Calendar.SECOND, new Preferences().EoDEMA_Time_Second);     // Gets the seconds of the day from the preference.
 
-        try
+        try     // Try the scheduled task.
         {
             long delay = calendar.getTimeInMillis() - System.currentTimeMillis();       // Starts a long delay variable.
 
@@ -37,7 +39,9 @@ public class EODTimerService extends Application        // Starts the EOD EMA Ti
                 @Override
                 public void run()       // Runs when it is called.
                 {
-                    String data =  ("End of Day Timer Service started prompt 1 at " + new SystemInformation().getTimeStamp());       // This is the format it is logged at.
+                    Log.i("End of Day EMA", "End of Day EMA Timer is starting First EMA Prompt");     // Logs on Console.
+
+                    String data =  ("End of Day Timer Service started Prompt 1 at " + new SystemInformation().getTimeStamp());       // This is the format it is logged at.
                     DataLogger datalog = new DataLogger("Sensor_Activity.csv",data);      // Logs it into a file called System Activity.
                     datalog.LogData();      // Saves the data into the directory.
 
@@ -46,7 +50,7 @@ public class EODTimerService extends Application        // Starts the EOD EMA Ti
                 }
             }, delay, new Preferences().EoDEMA_Period);     // Gets the preferences setting from the preference system.
         }
-        catch(Exception ex)
+        catch(Exception ex)     // If it fails manually start the service.
         {
             long delay = calendar.getTimeInMillis() - System.currentTimeMillis() + 24*60*60*1000;       // If it failed, start it manually.
 
@@ -56,7 +60,9 @@ public class EODTimerService extends Application        // Starts the EOD EMA Ti
                 @Override
                 public void run()       // Runs when it is called.
                 {
-                    String data =  ("End of Day Timer Service started prompt 1 at " + new SystemInformation().getTimeStamp());       // This is the format it is logged at.
+                    Log.i("End of Day EMA", "End of Day EMA Timer is starting First EMA Prompt");     // Logs on Console.
+
+                    String data =  ("End of Day Timer Service started Prompt 1 at " + new SystemInformation().getTimeStamp());       // This is the format it is logged at.
                     DataLogger datalog = new DataLogger("Sensor_Activity.csv",data);      // Logs it into a file called System Activity.
                     datalog.LogData();      // Saves the data into the directory.
 
