f206c10b3fed4b7008dbbace442529a8bfd1211a
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-06-13 16:22:44 -0400

    Updated Low Battery Screen.

--- app/src/main/java/com/linklab/INERTIA/besi_c/LowBattery.java
+++ app/src/main/java/com/linklab/INERTIA/besi_c/LowBattery.java
@@ -21,26 +21,32 @@ public class LowBattery extends WearableActivity
         super.onCreate(savedInstanceState);     // Makes the screen and saves the instance
         setContentView(R.layout.activity_low_battery);      // Sets the view to show the low battery screen
 
-        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);      // Sets the vibrator service.
-        vibrator.vibrate(vibrationDuration);        // Sets the system to vibrate for that long.
-        Button dismiss = findViewById(R.id.Dismiss);        // Sets the dismiss button
-
-        String data =  ("Low Battery," + "Started at," + SystemInformation.getTimeStamp());       // This is the format it is logged at.
-        DataLogger datalog = new DataLogger(Subdirectory_DeviceLogs, System, data);      // Logs it into a file called System Activity.
-        datalog.LogData();      // Saves the data into the directory.
-
-        dismiss.setOnClickListener(new View.OnClickListener() // Waits for the dismiss button to be clicked.
+        if (SystemInformation.isSystemCharging(this))       // If the watch is currently charging,
         {
-            @Override
-            public void onClick(View v)         // When the button is clicked
-            {
-                String data =  ("Low Battery," + "Dismissed at," + SystemInformation.getTimeStamp());       // This is the format it is logged at.
-                DataLogger datalog = new DataLogger(Subdirectory_DeviceLogs, System, data);      // Logs it into a file called System Activity.
-                datalog.LogData();      // Saves the data into the directory.
+            finish();       // End
+        }
+        else    // If the system is not charging
+        {
+            Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);      // Sets the vibrator service.
+            vibrator.vibrate(vibrationDuration);        // Sets the system to vibrate for that long.
+            Button dismiss = findViewById(R.id.Dismiss);        // Sets the dismiss button
 
-                finish();       // It stops the class and the buzzing
-            }
-        });
-        setAmbientEnabled();            // Enables Always-on
-    }
+            String data =  ("Low Battery," + "Started at," + SystemInformation.getTimeStamp());       // This is the format it is logged at.
+            DataLogger datalog = new DataLogger(Subdirectory_DeviceLogs, System, data);      // Logs it into a file called System Activity.
+            datalog.LogData();      // Saves the data into the directory.
+
+            dismiss.setOnClickListener(new View.OnClickListener() // Waits for the dismiss button to be clicked.
+            {
+                @Override
+                public void onClick(View v)         // When the button is clicked
+                {
+                    String data =  ("Low Battery," + "Dismissed at," + SystemInformation.getTimeStamp());       // This is the format it is logged at.
+                    DataLogger datalog = new DataLogger(Subdirectory_DeviceLogs, System, data);      // Logs it into a file called System Activity.
+                    datalog.LogData();      // Saves the data into the directory.
+
+                    finish();       // It stops the class and the buzzing
+                }
+            });
+            setAmbientEnabled();            // Enables Always-on
+        }}
 }
