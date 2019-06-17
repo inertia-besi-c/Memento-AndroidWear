3bc3585b60969afccfc10ddff3d1bf618dce3fee
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-06-17 01:44:40 -0400

    Reset Values

--- app/src/main/java/com/linklab/INERTIA/besi_c/Preferences.java
+++ app/src/main/java/com/linklab/INERTIA/besi_c/Preferences.java
@@ -8,9 +8,9 @@ public class Preferences        // System wide one stop place to set all setting
     /* ------------------------------------------------------------------------------- Settings for Deployment, Read Notes Carefully ------------------------------------------------------------------------------*/
 
     // There should be **NO CHARACTERS OTHER THAN LETTERS, NUMBERS, - or _ ** in file or directory names!
-    public String DeviceID = "Patient";        // Internal ID of Device assigned to Dyad
-    public String DeploymentID = "DEMONSTRATION";      // Deployment ID
-    public String Role = "PT";         // Role of user wearing the watch; CG for Caregiver or PT for Patient
+    public String DeviceID = "DEVICE-IDENTIFICATION";        // Internal ID of Device assigned to Dyad
+    public String DeploymentID = "P3D1-V";      // Deployment ID
+    public String Role = "CG";         // Role of user wearing the watch; CG for Caregiver or PT for Patient
     public String Directory = Environment.getExternalStorageDirectory() + "/BESI-C/";        // Directory on the watch where all files are saved
 
     // Settings for Vibration | Time is in ms |
