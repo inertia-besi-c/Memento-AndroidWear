a0f6e5cb2849f76d5e58ec9efe5a0db3e2c5e580
Emmanuel Ogunjirin <eao5xc@virginia.edu>
2019-12-06 01:00:56 -0500

    Initiated Sleep only Screen in Manifest

---
+++ app/src/main/java/com/linklab/INERTIA/besi_c/MainActivitySleep.java
@@ -0,0 +1,18 @@
+package com.linklab.INERTIA.besi_c;
+
+// Imports
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+
+
+/* ************************************************************************************* MAIN ACTIVITY OF THE APP ************************************************************************************************** */
+public class MainActivitySleep extends WearableActivity  // This is the activity that runs on the main screen. This is the main User interface and dominates the start of the app
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)      // This is created on startup
+    {
+        super.onCreate(savedInstanceState);      // Creates the main screen.
+        setContentView(R.layout.activity_main_sleep);     // This is where the texts and buttons seen were made. (Look into: res/layout/activity_main_sleep)
+
+    }
+}
